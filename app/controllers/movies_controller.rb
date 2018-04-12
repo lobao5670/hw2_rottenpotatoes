2a1be792f68ef154dc38aef5298c63d5d5bc8179
lobao5670 <lobao5670@gmail.com>
2018-04-11 21:26:35 -0300

    rottenpotatoes

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,42 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+
+  def new
+
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :release_date)
+  end
+end
